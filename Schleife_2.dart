2c3d0b88197d1a6fa53e4f07b04f952122788470
MuratKocak541 <murat.kocal1@icloud.com>
2025-03-10 12:46:09 +0100

    der 2. commit von Murat

--- Schleife_2.dart
+++ Schleife_2.dart
@@ -1,7 +1,13 @@
-void main () {
-  int i = 0;
-  while (i < 10) {
-    print(i);
-    i++;
+void main() {
+  // Schleife von 1 bis 100
+  for (int i = 1; i <= 100; i++) {
+    // Überprüfe die Bedingung und gib die entsprechende Ausgabe
+    if (i < 10) {
+      print("Kleine Zahl: $i");
+    } else if (i < 60) {
+      print("Mittlere Zahl: $i");
+    } else {
+      print("Große Zahl: $i");
+    }
   }
 }
